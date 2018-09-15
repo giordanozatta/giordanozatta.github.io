63bf4036037d72ae93557cfb8e066f572f1c5e87
Giordano <special.giordano@hotmail.com>
2018-09-15 22:19:02 +1200

    cleaned up
    cleaned up

--- js/main.js
+++ js/main.js
@@ -10,20 +10,6 @@ $('.link2').click(function(){
     window.location.assign('contact.html')
 })
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
-}
+$(function() {
+    $('body').removeClass('fade-out');
+});
